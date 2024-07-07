d7dda9fe735a4ca4b45c23f1d03bffb41647417a
szymptom <bpodkolzin@kse.org.ua>
2024-07-08 02:19:43 +0300

    new project & added enum

---
+++ EntityFrameworkCoffeShop/EntityFrameworkCoffeShop/CoffeeShopMenu/MenuOptionsEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoffeeShop.CoffeeShopMenu
+{
+    public enum MenuOptionsEnum
+    {
+        AddProduct = 1,
+        RemoveProduct,
+        UpdateProduct,
+        ShowProduct,
+        ShowAllProducts,
+        Exit
+    }
+}
